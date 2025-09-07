6f99e759d399b6760264fe114e1e3a029d36740c
IrfanNaikwade <irfannayakvade2230@gmail.com>
2025-09-07 22:28:14 +0530

    removed unneccessary files

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Landing from "./Landing";
-
-const Index = () => {
-  return <Landing />;
-};
-
-export default Index;
